0b8d2488ea4828d459ac9807d05c5a253f1eb572
tonnycoder001 <tonnykahihia@gmail.com>
2024-10-17 14:02:03 +0300

    Create `todo-list` blade file to show all created todos

---
+++ resources/views/livewire/todo-list.blade.php
@@ -0,0 +1,16 @@
+<div>
+    @include('livewire.includes.create-todo-box')
+    @include('livewire.includes.search-box')
+
+
+    <div id="todos-list">
+        @foreach ($todos as $todo)
+            @include('livewire.includes.todo-card')
+        @endforeach
+
+        {{-- pagination --}}
+        <div class="my-2">
+            {{ $todos->links() }}
+        </div>
+    </div>
+</div>
